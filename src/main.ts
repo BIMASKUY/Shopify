fdc52497b0ce73696ef987cd0e84e58acae97293
Bima Rizqy Ramadhan <bimarizqyramadhan@gmail.com>
2025-01-18 05:06:14 +0700

    feat(talent-growth): give output in root to simplify check the server

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { RequestMethod } from '@nestjs/common';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
 
   await app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
